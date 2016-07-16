c60e27bfa24d8471a25758b18526a3a8ec3ced80
sungmin <ignocide@gmail.com>
2016-07-16 22:23:51 +0900

    type define, permit function add

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 "use strict";
 
+const TYPE = {
+  NUMBER      : "number",
+  ARRAY       : "array",
+  OBJECT      : "object",
+  NULL        : "null",
+  FUNCTION    : "function",
+  UNDEFINED   : "undefined",
+  STRING      : "string",
+  BOOLEAN     : "boolean"
+}
 // null           ->  Null
 // []             ->  Array
 // {}             ->  Object
@@ -9,7 +19,37 @@
 // undefined      ->  Undefined
 // function(){}   ->  Funtion
 var typeOf = function(object){
-    return Object.prototype.toString.call(object).replace("[", "").replace("]", "").replace("object ", "");
+  return Object.prototype.toString.call(object).replace("[", "").replace("]", "").replace("object ", "").toLowerCase();
 }
 
+
+var permit = function(obj,req){
+
+  //obj must be Object && com must be
+  // if(typeOf(obj) != TYPE.OBJECT || !(typeOf(req) == TYPE.OBJECT || typeOf(req) == TYPE.ARRAY)){
+
+  //this version array only
+  if(typeOf(obj) != TYPE.OBJECT || !(typeOf(req) == TYPE.ARRAY)){
+    return false;
+  }
+
+  //deep copy
+  var target = Object.assign({},obj);
+  var req = req.concat([],req);
+
+  for(var i = 0;i<req.length;i++){
+    delete target[req[i]]
+  }
+
+  if(Object.keys(target).length){
+    return false;
+  }
+  else{
+    return true;
+  }
+
+}
+
+
+exports = module.exports = permit;
 exports.typeOf = typeOf;
