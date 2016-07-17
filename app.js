1118f982262649f4768116a9b8103b34fe8fba05
sungmin <ignocide@gmail.com>
2016-07-17 22:59:30 +0900

    suppert guide object type

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const TYPE = {
   STRING      : "string",
   BOOLEAN     : "boolean"
 }
+
+const TYPEARR = ["number","array","object","null","function","undefined","boolean"];
 // null           ->  Null
 // []             ->  Array
 // {}             ->  Object
@@ -26,20 +28,75 @@ var typeOf = function(object){
 var permit = function(obj,req){
 
   //obj must be Object && com must be
-  // if(typeOf(obj) != TYPE.OBJECT || !(typeOf(req) == TYPE.OBJECT || typeOf(req) == TYPE.ARRAY)){
+  if(typeOf(obj) != TYPE.OBJECT || !(typeOf(req) == TYPE.OBJECT || typeOf(req) == TYPE.ARRAY)){
 
   //this version array only
-  if(typeOf(obj) != TYPE.OBJECT || !(typeOf(req) == TYPE.ARRAY)){
+  // if(typeOf(obj) != TYPE.OBJECT || !(typeOf(req) == TYPE.ARRAY)){
+
     return false;
   }
 
   //deep copy
   var target = Object.assign({},obj);
-  var req = req.concat([],req);
 
-  for(var i = 0;i<req.length;i++){
-    delete target[req[i]]
+  //make guide
+  //case array;
+  if(typeOf(req) == TYPE.ARRAY){
+
+    for(var i = 0;i<req.length;i++){
+      delete target[req[i]]
+    }
+
   }
+
+  //case object
+  else{
+    var guide = {};
+    var guide_keys
+
+    guide_keys = Object.keys(req);
+
+    // for(var i = 0;i<guide_keys.length;i++){
+    //   if(req[guide_keys[i]] && req[guide_keys[i]]["type"] && TYPEARR.indexOf(req[guide_keys[i]]) != -1){
+    //     guide[guide_keys] =
+    //   }
+    //   else{
+    //     guide[guide_keys] = {
+    //       type : "ignore"
+    //     }
+    //   }
+    //
+    // }//end for
+
+    for(var i = 0; i<guide_keys.length;i++){
+
+      //있어야한다. 없으면 끝
+      var key = guide_keys[i]
+      if(target[key]){
+
+        //숫자일 경우 숫자로 컨버팅
+        var targetType = typeOf(target[key])
+        var reqType = req[key]
+
+        if(reqType == TYPE.NUMBER && !isNaN(parseInt(target[key]))){
+          target[key] = parseInt(target[key])
+        }
+
+
+        if(targetType == req[key]){
+          delete target[key];
+        }
+        else{
+          break;
+        }
+      }
+      else{
+        break;
+      }//end if
+    }
+  }
+
+
 
   if(Object.keys(target).length){
     return false;
