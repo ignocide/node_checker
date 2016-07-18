0779246ca80ffdfb07b19111fb3e5a35dc082aa3
sungmin <ignocide@gmail.com>
2016-07-18 23:01:46 +0900

    req supply object,
    opt = type,toNumber,allowKeyNull,allowValueNull
    
    Signed-off-by: sungmin <ignocide@gmail.com>

--- app.js
+++ app.js
@@ -20,6 +20,16 @@ const TYPEARR = ["number","array","object","null","function","undefined","boolea
 // "!"            ->  String
 // undefined      ->  Undefined
 // function(){}   ->  Funtion
+const GUIDEOPTIONS = {
+  type : null,
+  toNumber : true,
+  // func : null,
+  allowValueNull : false,
+  allowKeyNull : false
+}
+
+
+const GUIDEKEYS = Object.keys(GUIDEOPTIONS);
 var typeOf = function(object){
   return Object.prototype.toString.call(object).replace("[", "").replace("]", "").replace("object ", "").toLowerCase();
 }
@@ -29,74 +39,138 @@ var permit = function(obj,req){
 
   //obj must be Object && com must be
   if(typeOf(obj) != TYPE.OBJECT || !(typeOf(req) == TYPE.OBJECT || typeOf(req) == TYPE.ARRAY)){
-
-  //this version array only
-  // if(typeOf(obj) != TYPE.OBJECT || !(typeOf(req) == TYPE.ARRAY)){
-
     return false;
   }
 
-  //deep copy
-  var target = Object.assign({},obj);
+  let reqType = typeOf(req);
 
-  //make guide
-  //case array;
-  if(typeOf(req) == TYPE.ARRAY){
+  //copy of req
+  let request = {};
+  //use compare
+  let guide = {};
+  //use compare keys
+  let guideKey = [];
+  //request compares key
+  let reqKeys = [];
 
+
+  //array to object
+  if(reqType == TYPE.ARRAY){
+    request = {};
     for(var i = 0;i<req.length;i++){
-      delete target[req[i]]
+      request[req[i]] = { type : null };
     }
+    reqType = TYPE.OBJECT;
+  }
+  else if(reqType == TYPE.OBJECT){
+    request = Object.assign(req);
+  }
+
+  if(reqType == TYPE.OBJECT){
+      reqKeys = Object.keys(request);
+
+
+      //use because "break"
+      for(var i =0;i<reqKeys.length;i++){
+      // reqKey.forEach(function(key){
+        let key = reqKeys[i];
+
+        guide[key] = Object.assign({},GUIDEOPTIONS);
+        // guide[key] = TYPEARR.indexOf(req[type] != -1) ? req[type] : null
+        let reqItemOpt = request[key];
+        if(typeOf(reqItemOpt) == TYPE.STRING){
+          if(TYPEARR.indexOf(reqItemOpt) != -1){
+            guide[key]["type"] = reqItemOpt
+          }
+          else{
+            return false;
+          }
+        }
+
+
+        else if(typeOf(reqItemOpt) == TYPE.OBJECT){
+          let reqOptKeys = Object.keys(reqItemOpt);
+          for(var j=0;j<reqOptKeys.length;j++){
+            let reqOptKey = reqOptKeys[i];
+            let reqOptValue = reqItemOpt[reqOptKey];
+            let reqOptValueType = typeOf(reqOptValue);
+
+            //todo : make condition object and make dynamic comapre
+            if(reqOptKey == "type" && !(TYPEARR.indexOf(reqOptValueType) > 0 || reqOptValueType == null)){
+              return false;
+            }
+            else if(reqOptKey == "toNumber" && reqOptValueType != TYPE.BOOLEAN){
+              return false;
+            }
+            // else if(reqOptKey == "func" && reqOptValueType != TYPE.FUNCTION){
+            //   return false;
+            // }
+            else if(reqOptKey == "allowValueNull" && reqOptValueType != TYPE.BOOLEAN){
+              return false;
+            }
+            else if(reqOptKey == "allowKeyNull" && reqOptValueType != TYPE.BOOLEAN){
+              return false;
+            }
+            else{
+              guide[key][reqOptKey] = reqItemOpt[reqOptKey];
+            }
+          }
+        }
+        else{
+          return false;
+        }
+      }
 
   }
 
-  //case object
-  else{
-    var guide = {};
-    var guide_keys
+  //deep copy
+  let target = Object.assign({},obj);
+  let guideKeys = Object.keys(guide);
 
-    guide_keys = Object.keys(req);
 
-    // for(var i = 0;i<guide_keys.length;i++){
-    //   if(req[guide_keys[i]] && req[guide_keys[i]]["type"] && TYPEARR.indexOf(req[guide_keys[i]]) != -1){
-    //     guide[guide_keys] =
-    //   }
-    //   else{
-    //     guide[guide_keys] = {
-    //       type : "ignore"
-    //     }
-    //   }
-    //
-    // }//end for
 
-    for(var i = 0; i<guide_keys.length;i++){
 
-      //있어야한다. 없으면 끝
-      var key = guide_keys[i]
-      if(target[key]){
+  //compare!
+  for(var i = 0; i<guideKeys.length;i++){
 
-        //숫자일 경우 숫자로 컨버팅
-        var targetType = typeOf(target[key])
-        var reqType = req[key]
 
-        if(reqType == TYPE.NUMBER && !isNaN(parseInt(target[key]))){
-          target[key] = parseInt(target[key])
+    //있어야한다. 없으면 끝
+    var key = guideKeys[i];
+    var guideOpt = guide[key];
+    if(target[key]){
+      //숫자일 경우 숫자로 컨버팅
+      var targetType = typeOf(target[key])
+      let guideType = guideOpt['type']
+
+
+      if(guideType == TYPE.NUMBER && !isNaN(parseInt(target[key]))){
+        target[key] = parseInt(target[key]);
+
+        if(guideOpt.toNumber){
+          obj[key] = parseInt(obj[key]);
         }
+      }
 
 
-        if(targetType == req[key]){
-          delete target[key];
-        }
-        else{
-          break;
-        }
+      let pass = true;
+      if(targetType == guideType || guideType == null){
+      }
+      else if(guideOpt.allowValueNull){
       }
       else{
         break;
-      }//end if
+      }
+
+      if(pass){
+        delete target[key];
+      }
     }
+    else if(guideOpt.allowKeyNull){
+    }
+    else{
+      break;
+    }//end if
   }
-
-
 
   if(Object.keys(target).length){
     return false;
